9391bdefbefc0d1455aab3546f4b501ea4e736e6
Heclypso <heclypso@gmail.com>
2025-06-03 21:40:11 -0300

    Desenvolvido os containers About, Projects e Sidebar, desenvolvido o componente title

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,17 @@
+import Title from '../../components/Title'
+
+const About = () => {
+  return (
+    <section>
+      <Title>Sobre mim</Title>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
+        similique eveniet fugit nulla maxime autem quibusdam architecto
+        inventore animi. Obcaecati excepturi vero soluta debitis quis, alias
+        nemo est facilis harum?
+      </p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <img src="https://github.com/heclypso.png" alt="" />
+      <Title>Gabriel Munhak</Title>
+    </aside>
+  )
+}
+
+export default Sidebar
