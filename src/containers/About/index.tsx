5ec7b41445a827d4c30a34ac848c2015482a9667
Heclypso <heclypso@gmail.com>
2025-06-04 21:09:29 -0300

    Desenvolvido componentes de título e parágrafo

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,15 +1,16 @@
 import Title from '../../components/Title'
+import Paragraph from '../../components/Paragraph'
 
 const About = () => {
   return (
     <section>
-      <Title>Sobre mim</Title>
-      <p>
+      <Title fontSize={16}>Sobre mim</Title>
+      <Paragraph type="secondary">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
         similique eveniet fugit nulla maxime autem quibusdam architecto
         inventore animi. Obcaecati excepturi vero soluta debitis quis, alias
         nemo est facilis harum?
-      </p>
+      </Paragraph>
     </section>
   )
 }
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <aside>
       <img src="https://github.com/heclypso.png" alt="" />
-      <Title>Gabriel Munhak</Title>
+      <Title fontSize={20}>Gabriel Munhak</Title>
     </aside>
   )
 }
