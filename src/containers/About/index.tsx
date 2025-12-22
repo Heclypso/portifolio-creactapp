a78201f36e1463f9041de283416b245a83e3a4ca
Heclypso <heclypso@gmail.com>
2025-12-22 16:22:01 -0300

    #42 chore: alterado o texto que é exibido no parágrafo sobre mim.

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -8,60 +8,69 @@ const About = () => {
     <section id="about">
       <Title>Sobre mim</Title>
       <Paragraph type="tertiary">
-        Olá, eu me chamo Gabriel e tenho 21 anos.
+        Meu nome é Gabriel, tenho 21 anos e no nono ano do ensino fundamental,
+        isso em 2018 eu descobri o mundo do TI e a programação, sempre quis
+        seguir uma carreira de TI e sempre gostei de estudar, então construir
+        uma carreira casou como uma luva para mim.
         <br />
         <br />
-        No nono ano do ensino fundamental, em 2018, eu descobri o mundo do TI e
-        da programação. Sempre quis seguir uma carreira nessa área e sempre
-        gostei de estudar, então construir uma carreira em TI casou como uma
-        luva para mim.
+        Em 2019, comecei o ensino médio na ETEC da minha cidade e integrado ao
+        ensino médio cursei Análise e Desenvolvimento de Sistemas. Passar na
+        prova da ETEC pra mim foi incrível, me senti como se tivesse dado o
+        primeiro passo na minha carreira.
         <br />
         <br />
-        Em 2019, comecei o ensino médio na ETEC da minha cidade e cursei Análise
-        e Desenvolvimento de Sistemas integrado ao ensino médio. Passar na prova
-        da ETEC foi incrível.
+        Depois do ensino médio tirei um tempo para pensar em qual vertente da
+        programação investiria minha vida, então busquei um emprego de meio
+        período para pagar um curso profissionalizante de Engenheiro Front-end
+        na EBAC. No curso, desenvolvi projetos que utilizavam tecnologias como,
+        HTML5, CSS3, Javascript, Babel, Typescript, JQuery, React, Vite, Redux,
+        Sass, Less, Styled-components, Gulp, Cypress, Jest, Bootstrap, Git e
+        GitHub. Além disso também pratiquei a conceitos como consumo de APIs
+        RESTful, desenvolvimento de interfaces responsivas e deploy de
+        aplicações utilizando a Vercel.
         <br />
         <br />
-        Depois do ensino médio, tirei um tempo para pensar em qual vertente da
-        programação eu investiria minha vida. Nesse período, busquei um emprego
-        de meio período para pagar um curso profissionalizante de Engenheiro
-        Front-end na EBAC.
+        Fui contratado como Jovem Aprendiz na Ypê através de uma empresa
+        terceirizada chamada Espro e comecei a usar o dinheiro do meu salário
+        para pagar o curso.
         <br />
         <br />
-        No curso, desenvolvi projetos utilizando tecnologias como HTML5, CSS3,
-        Javascript, Babel, Typescript, JQuery, React, Vite, Redux, Sass, Less,
-        Styled-components, Gulp, Cypress, Jest, Bootstrap, Git e GitHub. Além
-        disso, também pratiquei conceitos como consumo de APIs RESTful,
-        desenvolvimento de interfaces responsivas e deploy de aplicações
-        utilizando a Vercel.
+        Dentro da Ypê trabalhei em um escritório de Manutenção, eu dava meu
+        melhor todos os dias, gerenciar tempo sempre foi um ponto forte meu,
+        então sempre entreguei minhas atribuições dentro do prazo. No começo,
+        foi um choque, porque eu não entendia nada sobre manutenção. Mas minha
+        comunicação sempre foi meu ponto forte, então foi só questão de tempo
+        para dominar as demandas que me eram passadas.
         <br />
         <br />
-        Fui contratado como Jovem Aprendiz na Ypê através da Espro e comecei a
-        usar o dinheiro do meu salário para pagar o curso. Trabalhei em um
-        escritório de manutenção, sempre entregando minhas atribuições no prazo
-        e usando minha comunicação como ponto forte para me adaptar rapidamente
-        mesmo sem conhecer a área no início.
+        Ainda dentro da Ypê fui o primeiro menor aprendiz do meu setor à
+        participar de um projeto. Dentro da minha turma da Espro acontecia uma
+        empresa simulada dividida em setores, logo quando entrei fui designado
+        para o setor de TI, após isso a minha instrutora fez uma reestruturação
+        na empresa e resolvi me candidatar como membro da diretoria e então,
+        quando o resultado chegou ela tinha me escolhido como consultor da
+        diretoria, eu só ficava abaixo dela na hierarquia da empresa.
         <br />
         <br />
-        Fui o primeiro menor aprendiz do meu setor a participar de um projeto.
-        Dentro da minha turma da Espro, participei de uma empresa simulada e fui
-        designado para o setor de TI. Após uma reestruturação, me candidatei
-        para a diretoria e fui escolhido como consultor, ficando logo abaixo da
-        instrutora na hierarquia.
+        Ao fim do meu contrato, como resultado do meu trabalho, minha instrutora
+        me entregou um certificado de destaque pelo meu desempenho.
         <br />
         <br />
-        Ao fim do meu contrato, recebi um certificado de destaque pelo meu
-        desempenho.
+        Sei que além de trabalho em equipe e ferramentas, ser Programador é,
+        antes de tudo, sobre resolver problemas. Por isso, minha capacidade de
+        resolver problemas sempre foi meu maior diferencial – seja aprendendo
+        novas tecnologias ou praticando as que eu já sei.
         <br />
         <br />
-        Sei que, além de ferramentas e trabalho em equipe, ser programador é
-        antes de tudo sobre resolver problemas. Minha criatividade,
-        adaptabilidade, resiliência e capacidade de aprender sempre foram meus
-        diferenciais.
-        <br />
-        <br />
-        Agora, quero ir além. Busco um ambiente desafiador para aprender e
-        crescer profissionalmente em minha carreira.
+        Tenho muita criatividade para pensar fora da caixa e encontrar soluções
+        inovadoras, mas sem perder o foco nos requisitos, resultados e impactos
+        da minha escolha. Sou adaptável, porque sei que o mundo da programação
+        muda o tempo todo e o que funciona hoje pode não funcionar amanhã. E,
+        acima de tudo, sou resiliente – já aprendi na prática que um desafio bem
+        enfrentado sempre vira uma grande oportunidade de crescimento. Agora,
+        quero ir além. Busco um ambiente desafiador para aprender e crescer
+        profissionalmente em minha carreira.
       </Paragraph>
       <GithubSection>
         <img src="https://github-readme-stats.vercel.app/api?username=heclypso&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
